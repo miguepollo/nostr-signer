7efd105cc1f88dd2ac0e1f9daa85862a7e14247c
miguepollo <miguelgarciapel@protonmail.com>
2023-06-16 17:47:18 +0200

    Borrado de mirar el swap.

--- src/main.cpp
+++ src/main.cpp
@@ -52,8 +52,6 @@ NostrQueueProcessor nostrQueue;
 void logMemory(){
   log_d("Total heap: %d", ESP.getHeapSize());
   log_d("Free heap: %d", ESP.getFreeHeap());
-  log_d("Total PSRAM: %d", ESP.getPsramSize());
-  log_d("Free PSRAM: %d", ESP.getFreePsram());
 }
 
 
