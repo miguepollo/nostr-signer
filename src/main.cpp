5e52d4dbcc2ab85cb4529e00d82288df382b180d
miguepollo <miguelgarciapel@protonmail.com>
2023-10-23 17:37:06 +0200

    Add led colour changing

--- src/main.cpp
+++ src/main.cpp
@@ -262,6 +262,13 @@ void setup()
 
 void loop()
 {
+
+    auto r = (byte)(millis() / 75);
+    auto g = (byte)(millis() / 10);
+    auto b = (byte)(millis() / 150);
+
+    smartdisplay_set_led_color(lv_color32_t({.ch = {.blue = b, .green = g, .red = r}}));
+
     lv_timer_handler();
     // nostrRelayManager.loop();
     // nostrRelayManager.broadcastEvents();
